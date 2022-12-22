b001374b37f7def9a2975385420f92f2a34bc923
victorber21 <victorber21@gmail.com>
2022-12-22 10:57:11 -0300

    Initial commit

---
+++ inicio/src/main/java/com/projetoimc/inicio/InicioApplication.java
@@ -0,0 +1,14 @@
+package com.projetoimc.inicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InicioApplication.class, args);
+
+	}
+
+}
---
+++ inicio/src/test/java/com/projetoimc/inicio/InicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoimc.inicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
